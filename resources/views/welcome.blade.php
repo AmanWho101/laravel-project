a64c8bb9b97c7d62ff76ff6ccd6dfc12c70d0b5a
AmanWho101 <Amleaz2005@gmail.com>
2022-07-29 09:25:02 +0300

    laravel time

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,61 +8,111 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900$display=swap" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+       <link rel="stylesheet" href="style.css">
+       <style>
+           @import url("https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900$display=swap");
+    * {
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+         font-family: 'Poppins', sans-serif;
+     }
+     body{
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         min-height: 100vh;
+         background: #2f363e;
+     }
+     #time{
+         display: flex;
+         gap: 40px;
+         color: rgb(255, 255, 255);
+     }
+     #time .circle{
+         position: relative;
+         width: 150px;
+         height: 150px;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+     }
+     #time div{
+         position: absolute;
+         text-align: center;
+         font-weight: 500;
+         font-size: 1.5em;
+     }
+    #time .ap{
+         position: relative;
+         font-size: 1em;
+         transform: translateX(-20px);
+     }
+     #time .circle svg{
+         position: relative;
+         width: 150px;
+         height: 150px;
+         transform: rotate(270deg)
+     }
+     #time .circle svg circle{
+         
+         width: 100%;
+         height: 100%;
+         fill: transparent;
+         stroke: #191919;
+         stroke-width: 4;
+         transform: translate(5px,5px);
+     }
+     #time .circle svg circle:nth-child(2){
+         stroke: var(--clr);
+         stroke-dasharray: 440;
+     }
+    #time div span{
+        position: absolute;
+        transform: translateX(-50%) translateY(-10%);
+        font-size: 0.35em;
+        font-weight: 300;
+        letter-spacing: 0.1em;
+        text-transform: uppercase;
+    }
+     .dots {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+        display: flex;
+        justify-content: center;
+     }
+     .dots::before{
+        content: '';
+        position: absolute;
+        top: -3px;
+        width: 15px;
+        height: 15px;
+        background: var(--clr);
+        border-radius: 50%;
+        box-shadow: 0 0 20px var(--clr),
+        0 0 60px var(--clr);
+     }
 
-            .full-height {
-                height: 100vh;
-            }
+     .links > a {
+         color: #ffffff;
+         padding: 0 25px;
+         font-size: 12px;
+         font-weight: 600;
+         letter-spacing: .1rem;
+         text-decoration: none;
+         text-transform: uppercase;
+     }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+     .m-b-md {
+         margin-bottom: 30px;
+     }
+ 
+       </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,13 +129,104 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                <h3>Hellow World, from Laravel</h3>
-                    Laravel
-              
+                    <div id="time">
+                        <div class="circle" style="--clr:#0aff01;">
+                            <div class="dots hr_dot"></div>
+                            <svg>
+                                <circle cx="70" cy="70" r="70"></circle>
+                                <circle cx="70" cy="70" r="70" id="hh"></circle>
+        
+                            </svg>
+                            <div id="hours">
+                                00
+                            </div>
+                        </div>
+                        
+                        <div class="circle" style="--clr:#ffff01;">
+                            <div class="dots min_dot"></div>
+                            <svg>
+                                <circle cx="70" cy="70" r="70"></circle>
+                                <circle cx="70" cy="70" r="70" id="mm"></circle>
+        
+                            </svg>
+                            <div id="minutes">
+                                00
+                            </div>
+                        </div>
+                        
+                        <div class="circle" style="--clr:#ff0101;">
+                            <div class="dots ss_dot"></div>
+                            <svg>
+                                <circle cx="70" cy="70" r="70"></circle>
+                                <circle cx="70" cy="70" r="70" id="ss"></circle>
+        
+                            </svg>
+                            <div id="seconds">
+                                00
+                            </div> 
+                        </div>
+                        
+                        <div class="ap">
+                            <div id="ampm">
+                                AM
+                            </div>
+                        </div>
+                                
+                </div>
                 </div>
                 <div class="containor">
-                    
+                   
+                 
                 </div>
+
+                <script>
+                    setInterval(() => {
+                    let hours =document.getElementById('hours');
+                    let minutes =document.getElementById('minutes');
+                    let seconds =document.getElementById('seconds');
+                    let ampm =document.getElementById('ampm');
+                    
+                    let h = new Date().getHours();
+                    let m = new Date().getMinutes();
+                    let s = new Date().getSeconds();
+                    let am = h >=12 ? "PM" : "AM";
+                    if (h > 12){
+                        h = h - 12;
+                    }
+
+                    h = (h <10)?"0" + h : h;
+                    m = (m <10)?"0" + m : m;
+                    s  = (s <10)?"0" + s : s;
+
+                    hours.innerHTML = h + '<br><span>hours</span>';
+                    minutes.innerHTML = m  + '<br><span>minutes</span>';
+                    seconds.innerHTML = s  + '<br><span>seconds</span>';
+                    ampm.innerHTML = am;
+
+                    let hh = document.getElementById('hh');
+                    let mm = document.getElementById('mm');
+                    let ss = document.getElementById('ss');
+
+                    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+                    // 12 hrs clock 
+                    mm.style.strokeDashoffset =  440 - (440 * m) / 60;
+                    // 60 mm clock
+                    ss.style.strokeDashoffset =  440 - (440 * s) / 60;
+                    // 60 ss clock
+                    let hr_dot = document.querySelector('.hr_dot');
+                    let min_dot = document.querySelector('.min_dot');
+                    let ss_dot = document.querySelector('.ss_dot');
+
+                    hr_dot.style.transform = `rotate(${h * 30}deg)`;
+                    //360/12
+                    min_dot.style.transform = `rotate(${m * 6}deg)`;
+                    //360/60
+                    ss_dot.style.transform = `rotate(${s * 6}deg)`;
+                    //360/60
+                });
+                   
+                    
+                </script>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
